a3d07f6e75f362ea492523526f15f85b3019ab78
Alexander Bachschmid <info@100-designs.com>
2020-06-17 19:19:14 +0200

    test-route

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/api/podcasts', function(req, res, next) {
+  res.json([
+    {
+      a: 'b'
+    }
+  ])
+});
+
 module.exports = router;
